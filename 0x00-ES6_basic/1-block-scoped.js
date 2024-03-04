972128052ca14abc2c5db8d31912420e4a63bcbf
johnyREx <johnkwofie99@outlook.com>
2024-03-04 16:15:19 +0000

    task 1 fix

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-	let task = false; // useing let instead of var
-	let task2 = true; // use let instead of var
+  let task = false; // Use let instead of var
+  let task2 = true; // Use let instead of var
 
-	if (trueOrFalse) {
-		task = true; // Assign the value directly w/o redclaring
-		task2 = false; // Assign the value directly w/o redclaring
-	}
+  if (trueOrFalse) {
+    task = true; // Assign the value directly without re-declaring
+    task2 = false; // Assign the value directly without re-declaring
+  }
 
-	return [task, task2];
+  return [task, task2];
 }
