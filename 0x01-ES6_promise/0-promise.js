c8d7cbf7b556279e7873789daaeeaa4dabdb5934
johnyREx <johnkwofie99@outlook.com>
2024-03-05 15:05:01 +0000

    error fixes task 0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,4 +1,4 @@
 export default async function getResponseFromAPI() {
-	const promise = new Promise(() => []); // creating a new promise
+	const promise = new Promise(() => {}); // creating a new promise
 	return promise; // Returning a promise
 }
