3a30fd8946bc956741b4e9fb877a9919a6a9be56
johnyREx <johnkwofie99@outlook.com>
2024-03-06 10:02:30 +0000

    js is my life

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,3 +1,3 @@
 export default function uploadPhoto(filename) {
-	return new Promise((resolve, reject) => reject(new Error('${filename} cannot be processed')));
+  return new Promise((resolve, reject) => reject(new Error('${filename} cannot be processed')));
 }
