e4612d0ec02385e1a0779462d263bb6c92651ccb
johnyREx <johnkwofie99@outlook.com>
2024-03-06 09:44:33 +0000

    fixed eslint task 0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,4 +1,4 @@
 export default async function getResponseFromAPI() {
-	const promise = new Promise(() => {}); // creating a new promise
-	return promise; // Returning a promise
+  const promise = new Promise(() => {});
+  return promise;
 }
