05f8741e26b7f8e40e32505ca16101bb30ad219f
johnyREx <johnkwofie99@outlook.com>
2024-03-06 10:10:23 +0000

    final user error fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,15 +2,15 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-	const promises = [signUpUser(fisrtName, lastName), uploadPhoto(fileName)];
-	return Promise.allSettled(promises).then((res) => {
-		const array = [];
-		res.forEach((itemk) => {
-			const dict = {};
-			dict.status = item.status;
-			dict.value = (item.status === 'fulfilled') ? item.value : dict.value = String(item.reason);
-			array.push(dict);
-		});
-		return array;
-	});
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+  return Promise.allSettled(promises).then((res) => {
+    const array = [];
+    res.forEach((item) => {
+      const dict = {};
+      dict.status = item.status;
+      dict.value = (item.status === 'fulfilled') ? item.value : dict.value = String(item.reason);
+      array.push(dict);
+    });
+    return array;
+  });
 }
