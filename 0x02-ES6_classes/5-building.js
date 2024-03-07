7699aa4982639d1474769131b8c76d6db64fa9b6
johnyREx <johnkwofie99@icloud.com>
2024-03-07 14:16:29 +0000

    class extending building must override

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,13 @@
+export default class Building {
+  constructor(sqft = 0) {
+    if (this.constructor !== Building
+      && typeof this.evacuationWarningMessage !== 'function') {
+      throw new TypeError('Class extending Building must ovveride evacuationWarnningMessage');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
