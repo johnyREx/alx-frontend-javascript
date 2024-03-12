dbd8a5c7ab18a98c6c77beb7ec5d0ac663bcc0c8
johnyREx <johnkwofie99@icloud.com>
2024-03-12 10:31:43 +0000

    fixed a minor error

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,6 @@
 export default function createInt8TypedArray(length, position, value) {
   if (position < 0 || position >= length) {
-    throw new TypeError('Position outside range');
+    throw new Error('Position outside range');
   }
 
   const buffer = new ArrayBuffer(length);
